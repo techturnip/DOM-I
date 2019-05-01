95faee6a198b763e14ae3071ee63826c644ebdfb
Tyler Turnipseed <tyler@techturnip.us>
2019-05-01 16:31:52 -0400

    Added commenting for clarity, changed innerText to textContent, Navigation: changed For Loop to forEach, CTA: used .split/.join to add <br> tag set with .innerHTML to match the design file, changed .setAttribute(..) to '.src = ...', Main-Content: used Object.entries to create mainContentArr, used .forEach to split mainContentArr into two separate arrays, simplified For Loop, Contact: added comments for clarity

--- js/index.js
+++ js/index.js
@@ -46,12 +46,12 @@ const siteContent = {
 let logo = document.getElementById('logo-img');
 logo.setAttribute('src', siteContent['nav']['img-src']);
 
-// Navigation
+/* Navigation */
 let navItem = document.querySelectorAll('nav a');
 
-for (let i = 0; i < navItem.length; i++) {
-  navItem[i].innerText = siteContent.nav['nav-item-' + (i + 1)];
-}
+navItem.forEach(
+  (item, i) => (item.textContent = siteContent.nav['nav-item-' + (i + 1)])
+);
 
 /* CTA Section */
 
@@ -59,46 +59,43 @@ for (let i = 0; i < navItem.length; i++) {
 let ctaHeading = document.querySelector('.cta-text h1');
 let ctaBtn = document.querySelector('.cta-text button');
 let ctaImg = document.querySelector('#cta-img');
+let ctaHeadingArr = siteContent.cta.h1.split(' ');
 
-// Set innerText
-ctaHeading.innerText = siteContent.cta.h1;
-ctaBtn.innerText = siteContent.cta.button;
+// Set textContent of cta elements
+ctaHeading.innerHTML = ctaHeadingArr.join('<br>');
+ctaBtn.textContent = siteContent.cta.button;
 
-// Set src attribute
-ctaImg.setAttribute('src', siteContent.cta['img-src']);
+// Set src attribute for cta image
+ctaImg.src = siteContent.cta['img-src'];
 
 /* Main Content Section */
 
-// Query Selectors
+// Store main-content object to create an array
 let mainContent = siteContent['main-content'];
+
+// Query Selectors
 let mainContentHeadings = document.querySelectorAll('.text-content h4');
 let mainContentText = document.querySelectorAll('.text-content p');
 let mainContentImg = document.querySelector('#middle-img');
 
 // Store Object Values in Array for looping
-let mainContentHeadingArr = [
-  mainContent['features-h4'],
-  mainContent['about-h4'],
-  mainContent['services-h4'],
-  mainContent['product-h4'],
-  mainContent['vision-h4']
-];
-let mainContentTextArr = [
-  mainContent['features-content'],
-  mainContent['about-content'],
-  mainContent['services-content'],
-  mainContent['product-content'],
-  mainContent['vision-content']
-];
+let mainContentArr = Object.entries(mainContent);
+let mainContentHeadingArr = [];
+let mainContentTextArr = [];
 
-// Loop and apply innerText to elements from array
-for (
-  let i = 0;
-  i < mainContentHeadings.length && i < mainContentText.length;
-  i++
-) {
-  mainContentHeadings[i].innerText = mainContentHeadingArr[i];
-  mainContentText[i].innerText = mainContentTextArr[i];
+// Use forEach and .includes to create heading and content arrays
+mainContentArr.forEach(item => {
+  if (item[0].includes('h4')) {
+    mainContentHeadingArr.push(item[1]);
+  } else if (item[0].includes('content')) {
+    mainContentTextArr.push(item[1]);
+  }
+});
+
+// Loop through nodelist and set textContent to their respective array values
+for (let i = 0; i < mainContentHeadings.length; i++) {
+  mainContentHeadings[i].textContent = mainContentHeadingArr[i];
+  mainContentText[i].textContent = mainContentTextArr[i];
 }
 
 // Set src attribute for middle-img
@@ -108,18 +105,23 @@ mainContentImg.setAttribute(
 );
 
 /* Contact Section */
+
+// Query Selectors
 let contactHeading = document.querySelector('.contact h4');
 let contactParagraphs = document.querySelectorAll('.contact p');
 let contact = siteContent.contact;
 
+// Contact Array for looping through paragraphs
 let contactArr = [contact.address, contact.phone, contact.email];
 
-contactHeading.innerText = siteContent.contact['contact-h4'];
+// Set h4 to contact-h4
+contactHeading.textContent = siteContent.contact['contact-h4'];
 
+// Set p tags to contact address, phone and email
 for (let i = 0; i < contactParagraphs.length; i++) {
-  contactParagraphs[i].innerText = contactArr[i];
+  contactParagraphs[i].textContent = contactArr[i];
 }
 
 /* Footer */
 let copyright = document.querySelector('footer p');
-copyright.innerText = siteContent.footer.copyright;
+copyright.textContent = siteContent.footer.copyright;
